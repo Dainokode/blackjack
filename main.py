a99c0d6d5230a5d210e703fc294bfa2fb2c19583
Dainokode <antoniodainok@gmail.com>
2020-12-02 17:41:00 +0100

    final commit

--- main.py
+++ main.py
@@ -1,62 +1,95 @@
-############### Blackjack Project #####################
-
-# Difficulty Normal 😎: Use all Hints below to complete the project.
-# Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-# Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-# Difficulty Expert 🤯: Only use Hint 1 to complete the project.
-
-############### Our Blackjack House Rules #####################
-
-# The deck is unlimited in size.
-# There are no jokers.
-# The Jack/Queen/King all count as 10.
-# The the Ace can count as 11 or 1.
-# Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-# The cards in the list have equal probability of being drawn.
-# Cards are not removed from the deck as they are drawn.
-# The computer is the dealer.
-
-
-# MY CODE BELOW:
 import random
+from database import logo
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-user_cards = []
-computer_cards = []
+game_on = True
+
+while game_on:
+    # Logo and welcome message 
+    print(logo)
+    print("Welcome to Python BlackJack!\n")
+
+    def compare(u_score, c_score):
+        if u_score == c_score:
+            return "It's a draw"
+        elif c_score == 0 or c_score > u_score and c_score <= 21:
+            return "The computer wins"
+        elif u_score == 0 or u_score > c_score and u_score <= 21:
+            return "You win"
+        elif u_score > 21:
+            return "The computer wins"
+        elif c_score > 21:
+            return "You win"
 
 
-def deal_card():
-    return random.randint(0, len(cards))
+    def calculate_score(cards_list):
+        if len(cards_list) == 2 and sum(cards_list) == 21:
+            return 0
+
+        if 11 in cards_list and sum(cards_list) > 21:
+            cards_list.remove(11)
+            cards_list.append(1)
+
+        return sum(cards_list)
+
+    # Return a random card
+    def deal_card():
+        cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+        return random.choice(cards)
+
+    # User and computer's hands
+    user_cards = []
+    computer_cards = []
+
+    # Add two random cards to user's and computer's hands
+    for i in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    print(f"Your hand: {user_cards}\n")
+    print(f"Computer hand: {computer_cards}\n")
+
+    # sum up user's and computer's hands
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+
+    # print result
+    print(f"Your score is: {user_score}\n")
+    print(f"Computer score is {computer_score}\n")
+
+    is_game_over = False
+    while is_game_over == False:
+        if computer_score == 0 or user_score == 0 or user_score > 21 or computer_score > 21:
+            is_game_over = True
+        else:
+            user_choice = input("Do you want to draw another card? Type 'y' or 'n': ").lower()
+            if user_choice == "y":
+                # deal another card
+                user_cards.append(deal_card())
+                # recalculate the score
+                user_score = calculate_score(user_cards)
+                # print results
+                print(f"Your hand: {user_cards}\n")
+                print(f"Your score is: {user_score}\n")
+            else:
+                is_game_over = True
 
 
-user_cards.append(deal_card())
-user_cards.append(deal_card())
-computer_cards.append(deal_card())
-computer_cards.append(deal_card())
+    while computer_score != 0 and computer_score < 17:
+    # deal another card
+        computer_cards.append(deal_card())
+        # recalculate the score
+        computer_score = calculate_score(computer_cards)
+        # print results
+        print(f"Computer hand: {computer_cards}\n")
+        print(f"Computer score is {computer_score}\n")
+    else:
+        is_game_over = True
 
-print(f"User hand: {user_cards}")
-print(f"Computer hand: [{computer_cards[0]}]")
+    game_result = compare(user_score, computer_score)
 
+    print(game_result)
 
-##################### Hints #####################
+    restart_game = input("Do you want to play again? Type 'y' or 'n': ").lower()
+    if restart_game == "n":
+        game_on = False
 
-# Hint 6: Create a function called calculate_score() that takes a List of cards as input
-# and returns the score.
-# Look up the sum() function to help you do this.
-
-# Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-
-# Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-
-# Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-
-# Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-
-# Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-# Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
-# Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
-
-# Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
